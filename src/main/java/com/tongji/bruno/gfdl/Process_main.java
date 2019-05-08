01ad2d819e5c47012c5aa5b32cecdc7bd16e8dc2
Bruno Qin <brunoqin@163.com>
2019-05-08 15:22:45 +0800

    范数发展

--- src/main/java/com/tongji/bruno/gfdl/Process_main.java
+++ src/main/java/com/tongji/bruno/gfdl/Process_main.java
@@ -6,12 +6,42 @@ import ucar.nc2.Variable;
 public class Process_main {
 
     private static String TARGET = "/BIGDATA1/iocas_mmu_2/PPSO_qin/record/12_16_ocean.nc";
+    private static String ORIGIN_P = "/BIGDATA1/iocas_mmu_2/GFDL-CM2p1/output/04210101.ocean_month.nc";
+    private static String ORIGIN_N = "/BIGDATA1/iocas_mmu_2/GFDL-CM2p1/output/04260101.ocean_month.nc";
     private static String STANDARD = "/BIGDATA1/iocas_mmu_2/GFDL-CM2p1/output/sst_ave400Y.nc";
     private static String[] PARAMETERS = {"sst_jan_ave", "sst_feb_ave", "sst_mar_ave", "sst_april_ave", "sst_may_ave", "sst_june_ave", "sst_july_ave", "sst_agu_ave", "sst_sep_ave", "sst_oct_ave", "sst_nov_ave", "sst_dec_ave"};
 
     public static void main(String[] args) {
         Process_main m = new Process_main();
+        m.cnop_change(TARGET, ORIGIN_P);
         m.nino3_change(TARGET, STANDARD);
+    }
+
+    public void cnop_change(String target, String origin){
+        try{
+            NetcdfFile target_file = NetcdfFile.open(target);
+            NetcdfFile origin_file = NetcdfFile.open(origin);
+
+            Variable sst_target = target_file.findVariable("sst");
+            Variable sst_origin = origin_file.findVariable("sst");
+
+            double sum;
+
+            for(int time = 0; time < 12; time++){
+                sum = 0;
+                for(int i = 79; i < 129; i++){
+                    for(int j = 62; j < 129; j++){
+                        double sst_t = sst_target.read(time + ":" + time + ":1, " + i + ":" + i + ":1, " + j + ":" + j + ":1").getDouble(0);
+                        double sst_o = sst_origin.read(time + ":" + time + ":1, " + i + ":" + i + ":1, " + j + ":" + j + ":1").getDouble(0);
+                        sum += Math.pow((sst_t - sst_o), 2);
+                    }
+                }
+                System.out.println(time + ": " + Math.sqrt(sum));
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void nino3_change(String target, String standard){
